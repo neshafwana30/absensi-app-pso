c0b2b0ec6801b1aabeaaf5a3aed71e18de614a97
neshafwana30 <5026231013@student.its.ac.id>
2026-06-07 18:17:11 +0700

    Update driver session and cookie

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.base')
 
 @push('style')
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 @endpush
 
 @section('base')
+    @include('partials.navbar')
 
-@include('partials.navbar')
+    <div class="container-fluid">
+        <div class="row">
+            @include('partials.sidebar')
 
-<div class="container-fluid">
-    <div class="row">
-        @include('partials.sidebar')
-
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div
-                class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 class="h2">{{ $title }}</h1>
-                {{-- <div class="btn-toolbar mb-2 mb-md-0">
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div
+                    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">{{ $title }}</h1>
+                    {{-- <div class="btn-toolbar mb-2 mb-md-0">
                     <div class="btn-group me-2">
                         <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
                         <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
@@ -26,13 +25,13 @@
                         This week
                     </button>
                 </div> --}}
-                @yield('buttons')
-            </div>
+                    @yield('buttons')
+                </div>
 
-            <div class="py-4">
-                @yield('content')
-            </div>
-        </main>
+                <div class="py-4">
+                    @yield('content')
+                </div>
+            </main>
+        </div>
     </div>
-</div>
 @endsection
