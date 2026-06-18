f6b0293be964066e5eca01bc39ab066a562b3e0d
neshafwana30 <5026231013@student.its.ac.id>
2026-06-18 17:12:29 +0700

    update color

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,8 +29,7 @@
                     </label>
                 </div>
 
-                <button class="w-100 btn btn-primary" type="submit" id="login-form-button"
-                    style='background-color: #7a1257;'>Masuk</button>
+                <button class="w-100 btn btn-primary" type="submit" id="login-form-button">Masuk</button>
                 <p class="mt-5 mb-3 text-muted">&copy; 2022</p>
             </form>
         </main>
