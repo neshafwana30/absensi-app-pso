3a53946e6e7ee73fea9561c38dfe89c1d83e2a63
Jeremy Anggi <you@example.com>
2026-06-19 17:22:02 +0700

    Perubahan Radius didalam ITS

--- config/attendance.php
+++ config/attendance.php
@@ -4,14 +4,14 @@ return [
     
 
     // // Radius ITS
-    // 'office_latitude' => -7.280586, 
-    // 'office_longitude' => 112.796212, 
+    'office_latitude' => -7.280586, 
+    'office_longitude' => 112.796212, 
 
     // Radius Paper Cup Darhus
     // -7.2796370, 112.7713120 (laptop)
     // -7.2790031483251365, 112.77261389802882
-    'office_latitude' => -7.2790031483251365, 
-    'office_longitude' => 112.77261389802882, 
+    // 'office_latitude' => -7.2790031483251365, 
+    // 'office_longitude' => 112.77261389802882, 
     
     // Jarak radius maksimal yang diperbolehkan (dalam meter)
     // kalo sisfor 20 ya
